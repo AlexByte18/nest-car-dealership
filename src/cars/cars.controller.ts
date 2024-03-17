564d8060b94528b94622ad0cd0b2fea1f3b2358d
AlexByte <alex.g.cortes.g@gmail.com>
2024-03-16 23:04:48 -0600

    start cars module

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,22 @@
+import { CarsService } from './cars.service';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(
+        private readonly carsService: CarsService
+    ) {}
+
+    @Get()
+    getAllCars() {
+        return this.carsService.findAll();
+    }
+
+    @Get(':id')
+    getCarById(@Param('id', ParseIntPipe) id: number) {
+        console.log(id)
+        return this.carsService.findOneById(id);
+    }
+
+}
